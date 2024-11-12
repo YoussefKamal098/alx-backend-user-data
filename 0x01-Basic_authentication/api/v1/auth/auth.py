21e84e071367fb74bca6fd1024c9a0a114a681a5
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-12 21:05:11 +0200

    implemented require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
         """
-        return False
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        return path.rstrip('/') not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header
