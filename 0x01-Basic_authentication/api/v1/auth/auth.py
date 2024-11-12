a95fc9a7dfcb76c8be062e3d77f340acefaf7b0e
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-12 21:12:43 +0200

    updated authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,10 +22,10 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ authorization_header
         """
-        if not request:
+        if request is None or 'Authorization' not in request.headers:
             return None
 
-        return request.headers.get('Authorization', None)
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
