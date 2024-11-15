19016a456e9489a6abedc7a9dd0474916c86d1ad
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-15 20:22:02 +0200

    use flask.current_app.config to get SESSION_NAME env value

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,6 +15,7 @@ from api.v1.auth.auth_factory_provider import DefaultAuthFactoryProvider
 
 # Initialize Flask app and CORS
 app = Flask(__name__)
+app.config['SESSION_NAME'] = os.getenv('SESSION_NAME', '_my_session_id')
 
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -147,5 +147,5 @@ class Auth:
         if request is None:
             return None
 
-        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+        session_name = flask.current_app.config['SESSION_NAME']
         return request.cookies.get(session_name)
