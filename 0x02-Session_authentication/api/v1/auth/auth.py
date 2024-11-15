4ead038ad71368c5a5275301ea72ed03738592a0
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-15 18:56:01 +0200

    test

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -146,4 +146,4 @@ class Auth:
         if request is None:
             return None
 
-        return request.cookies.get('_cookie_cookie_dough')
+        return request.cookies.get('_hbnb_yelp_session_id')
