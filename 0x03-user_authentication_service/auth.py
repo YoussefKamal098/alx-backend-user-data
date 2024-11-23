cd0b035e11c14e841ff60e4e6dc69d54823289c3
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-23 15:11:29 +0200

    fixed register_user method to handle existing user check and raise ValueError

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,8 +76,10 @@ class Auth:
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError(f"User {email} already exists")
+            return self._db.add_user(
+                email=email, hashed_password=_hash_password(password)
+            )
 
-        return self._db.add_user(
-            email=email, hashed_password=_hash_password(password)
-        )
+        raise ValueError(f"User {email} already exists")
+
+
