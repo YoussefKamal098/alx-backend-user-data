ef0b07f280982634a4151052092425fb412bafd9
YoussefKamal098 <kmalywsf098@gmail.com>
2024-11-23 15:15:59 +0200

    fixed pep8 error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,10 +7,12 @@ from flask import Flask, jsonify
 # Initialize Flask app
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
     """Return a JSON response with a message"""
     return jsonify({"message": "Bienvenue"})
+
 
 # Run the app on host 0.0.0.0 and port 5000
 if __name__ == "__main__":
